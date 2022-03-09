97a8c34cbb41df46ac9f9c461c221d1aa83542b1
Kipperman, David <kippermand@gmail.com>
2022-03-09 16:27:36 -0500

    Tournament Model Added
    - unit tests

---
+++ source/business/Models/TournamentModel.cs
@@ -0,0 +1,38 @@
+﻿namespace NewEnglandClassic.Models;
+
+public class Tournament
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public DateOnly Start { get; set; }
+
+    public DateOnly End { get; set; }
+
+    public decimal EntryFee { get; set; }
+
+    public short Games { get; set; }
+
+    public decimal FinalsRatio { get; set; }
+
+    public decimal CashRatio { get; set; }
+
+    public string BowlingCenter { get; set; } = string.Empty;
+
+    public bool Completed { get; set; }
+
+    internal Tournament(Database.Entities.Tournament entity)
+    {
+        Id = entity.Id;
+        Name = entity.Name;
+        Start = entity.Start;
+        End = entity.End;
+        EntryFee = entity.EntryFee;
+        Games = entity.Games;
+        FinalsRatio = entity.FinalsRatio;
+        CashRatio = entity.CashRatio;
+        BowlingCenter = entity.BowlingCenter;
+        Completed = entity.Completed;
+    }
+}
