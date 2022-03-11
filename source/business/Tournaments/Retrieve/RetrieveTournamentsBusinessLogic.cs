f9d8f251a12783cf4f487444298e9ce2273e9e8c
Kipperman, David <kippermand@gmail.com>
2022-03-11 15:55:39 -0500

    Retrieve Tournaments BusinessLogic Added
    - unit tests

---
+++ source/business/Tournaments/Retrieve/RetrieveTournamentsBusinessLogic.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace NewEnglandClassic.Tournaments.Retrieve;
+public class BusinessLogic : IBusinessLogic
+{
+    public Models.ErrorDetail? ErrorDetail { get; private set; }
+
+    private readonly IDataLayer _dataLayer;
+
+    public BusinessLogic(IConfiguration config)
+    {
+        _dataLayer = new DataLayer(config);
+    }
+
+    /// <summary>
+    /// Unit Test Constructor
+    /// </summary>
+    /// <param name="mockDataLayer"></param>
+    internal BusinessLogic(IDataLayer mockDataLayer)
+    {
+        _dataLayer = mockDataLayer;
+    }
+
+    IEnumerable<Models.Tournament> IBusinessLogic.Execute()
+    {
+        try
+        {
+            return _dataLayer.Execute();
+        }
+        catch (Exception ex)
+        {
+            ErrorDetail = new Models.ErrorDetail(ex);
+            
+            return Enumerable.Empty<Models.Tournament>();
+        }
+    }
+}
+
+public interface IBusinessLogic
+{
+    Models.ErrorDetail? ErrorDetail { get; }
+
+    IEnumerable<Models.Tournament> Execute();
+}
