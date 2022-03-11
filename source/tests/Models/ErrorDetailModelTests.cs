84cb2020e7a274a1dd1d604d23d1a95cac0dafa3
Kipperman, David <kippermand@gmail.com>
2022-03-11 15:55:45 -0500

    remove unused imports

--- source/tests/Models/ErrorDetailModelTests.cs
+++ source/tests/Models/ErrorDetailModelTests.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NewEnglandClassic.Tests.Models;
+﻿namespace NewEnglandClassic.Tests.Models;
 
 [TestFixture]
 internal class ErrorDetailModelTests
