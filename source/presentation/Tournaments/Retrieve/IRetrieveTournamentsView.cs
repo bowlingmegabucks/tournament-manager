acd5d7545f77aa3dad23163fcb6c74dd7791d2f4
Kipperman, David <kippermand@gmail.com>
2022-03-20 13:45:56 -0400

    Retrieve Tournaments Presenter
    - unit tests

---
+++ source/presentation/Tournaments/Retrieve/IRetrieveTournamentsView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewEnglandClassic.Tournaments.Retrieve;
+
+public interface IView
+{
+    void DisplayErrorMessage(string message);
+
+    void DisableOpenTournament();
+
+    void BindTournaments(ICollection<IViewModel> viewModels);
+}
---
+++ source/presentation/Tournaments/Retrieve/RetrieveTournamentsPresenter.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewEnglandClassic.Tournaments.Retrieve;
+public class Presenter
+{
+    private readonly IView _view;
+
+    private readonly Lazy<IAdapter> _adapter;
+    private IAdapter Adapter => _adapter.Value;
+
+    public Presenter(IConfiguration config, IView view)
+    {
+        _view = view;
+
+        _adapter = new Lazy<IAdapter>(() => new Adapter(config));
+    }
+
+    /// <summary>
+    /// Unit Test Constructor
+    /// </summary>
+    /// <param name="mockView"></param>
+    /// <param name="mockAdapter"></param>
+    internal Presenter(IView mockView, IAdapter mockAdapter)
+    {
+        _view = mockView;
+        _adapter = new Lazy<IAdapter>(() => mockAdapter);
+    }
+
+    public void Execute()
+    {
+        var tournaments = Adapter.Execute();
+
+        if (Adapter.ErrorDetail != null)
+        {
+            _view.DisplayErrorMessage(Adapter.ErrorDetail.Message);
+            _view.DisableOpenTournament();
+        }
+        else if (!tournaments.Any())
+        {
+            _view.DisableOpenTournament();
+        }
+        else
+        {
+            _view.BindTournaments(tournaments.ToList());
+        }
+    }
+}
