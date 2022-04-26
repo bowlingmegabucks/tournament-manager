83ef9ec98502e57b9fc5046fd29d6ac40f67224d
Kipperman, David <kippermand@gmail.com>
2022-04-26 15:30:29 -0400

    Tournament Entity Mapper

---
+++ source/main/Tournaments/TournamentEntityMapper.cs
@@ -0,0 +1,25 @@
+﻿
+namespace NewEnglandClassic.Tournaments;
+
+internal class EntityMapper : IEntityMapper
+{
+    Database.Entities.Tournament IEntityMapper.Execute(Models.Tournament tournament)
+        => new()
+        {
+            Id = tournament.Id,
+            Name = tournament.Name,
+            Start = tournament.Start,
+            End = tournament.End,
+            BowlingCenter = tournament.BowlingCenter,
+            Games = tournament.Games,
+            FinalsRatio = tournament.FinalsRatio,
+            EntryFee = tournament.EntryFee,
+            CashRatio = tournament.CashRatio,
+            Completed = tournament.Completed
+        };
+}
+
+internal interface IEntityMapper
+{
+    Database.Entities.Tournament Execute(Models.Tournament tournament);
+}
