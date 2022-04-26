84b9480464df6cf87a8f914a5bf4da4b47f160e3
Kipperman, David <kippermand@gmail.com>
2022-04-26 15:38:24 -0400

    Add Tournament DO

---
+++ source/main/Tournaments/Add/AddTournamentDO.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace NewEnglandClassic.Tournaments.Add;
+
+internal class DataLayer : IDataLayer
+{
+    private readonly IEntityMapper _mapper;
+    private readonly IRepository _repository;
+
+    internal DataLayer(IConfiguration config)
+    {
+        _mapper = new EntityMapper();
+        _repository = new Repository(config);
+    }
+
+    /// <summary>
+    /// Unit Test Constructor
+    /// </summary>
+    /// <param name="mockMapper"></param>
+    /// <param name="mockRepository"></param>
+    internal DataLayer(IEntityMapper mockMapper, IRepository mockRepository)
+    {
+        _mapper = mockMapper;
+        _repository = mockRepository;
+    }
+
+    Guid IDataLayer.Execute(Models.Tournament tournament)
+    {
+        var entity = _mapper.Execute(tournament);
+        
+        return _repository.Add(entity);
+    }
+}
+
+internal interface IDataLayer
+{
+    Guid Execute(Models.Tournament tournament);
+}
