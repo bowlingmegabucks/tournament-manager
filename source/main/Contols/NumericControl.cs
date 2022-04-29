1947b7c09a40b29bc45f749ca143bf5ad021b459
Kipperman, David <kippermand@gmail.com>
2022-04-29 15:16:46 -0400

    General Controls added

---
+++ source/main/Contols/NumericControl.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace NewEnglandClassic.Controls;
+
+/// <summary>
+///
+/// </summary>
+public partial class NumericControl : NumericUpDown
+{
+    /// <summary>
+    ///
+    /// </summary>
+    public NumericControl()
+    {
+        InitializeComponent();
+        Enter += new EventHandler(SelectAll!);
+    }
+
+    private void SelectAll(object sender, EventArgs e)
+        => Select(0, Value.ToString().Length);
+}
---
+++ source/main/Contols/ProcessingMessage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace NewEnglandClassic.Controls;
+
+public partial class ProcessingMessage : UserControl
+{
+    private readonly CancellationTokenSource _cancelToken;
+
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="message"></param>
+    /// <param name="cancelToken"></param>
+    public ProcessingMessage(string message, CancellationTokenSource cancelToken)
+    {
+        InitializeComponent();
+        _cancelToken = cancelToken;
+
+        LabelMessage.Text = message;
+
+        if (string.IsNullOrWhiteSpace(message))
+        {
+            LabelMessage.Visible = false;
+            PictureBoxProcessing.Location = new Point(PictureBoxProcessing.Location.X, PictureBoxProcessing.Location.Y - LabelMessage.Height / 2);
+        }
+    }
+
+    private void ButtonCancel_Click(object sender, EventArgs e)
+        => _cancelToken.Cancel();
+}
