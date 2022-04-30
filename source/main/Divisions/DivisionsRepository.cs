f8cb38156446fac538ffd18640f6b6bdc7f86926
Kipperman, David <kippermand@gmail.com>
2022-04-30 10:16:29 -0400

    Divisions Repository

---
+++ source/main/Divisions/DivisionsRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace NewEnglandClassic.Divisions;
+
+internal class Repository : IRepository
+{
+    private readonly Database.IDataContext _dataContext;
+    internal Repository(IConfiguration config)
+    {
+        _dataContext = new Database.DataContext(config);
+    }
+
+    /// <summary>
+    /// Unit Test Constructor
+    /// </summary>
+    /// <param name="mockDataContext"></param>
+    internal Repository(Database.IDataContext mockDataContext)
+    {
+        _dataContext = mockDataContext;
+    }
+}
+
+internal interface IRepository
+{
+
+}
