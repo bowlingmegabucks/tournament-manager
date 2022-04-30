ca6f74d072c03a5690fec5983148b1014dc2a3cd
Kipperman, David <kippermand@gmail.com>
2022-04-30 11:23:22 -0400

    Division Entity Mapper

---
+++ source/main/Models/DivisionModel.cs
@@ -0,0 +1,28 @@
+﻿
+namespace NewEnglandClassic.Models;
+internal class Division
+{
+    public Guid Id { get; set; }
+
+    public short Number { get; set; }
+
+    public string Name { get; set; }
+
+    public Guid TournamentId { get; set; }
+
+    public short? MinimumAge { get; set; }
+
+    public short? MaximumAge { get; set; }
+
+    public int? MinimumAverage { get; set; }
+
+    public int? MaximumAverage { get; set; }
+
+    public decimal? HandicapPercentage { get; set; }
+
+    public int? HandicapBase { get; set; }
+
+    public int? MaximumHandicapPerGame { get; set; }
+
+    public Gender? Gender { get; set; }
+}
