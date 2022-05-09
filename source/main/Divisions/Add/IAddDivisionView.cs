de32a5daa294ebb1bf45c70a8c0637e25f63ee91
Kipperman, David <kippermand@gmail.com>
2022-05-09 14:00:36 -0400

    Add division view / preseenter shell

---
+++ source/main/Divisions/Add/IAddDivisionView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewEnglandClassic.Divisions.Add;
+internal interface IView
+{
+    bool IsValid();
+
+    IViewModel Division { get; }
+
+    void KeepOpen();
+
+    void DisplayErrors(IEnumerable<string> errors);
+
+    void DisplayMessage(string message);
+
+    void Close();
+}
