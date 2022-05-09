55c819524a1052842e2e92e3ff46099ae4e2f8d1
Kipperman, David <kippermand@gmail.com>
2022-05-09 14:47:03 -0400

    retrieve tournament divisions data layer / tests

---
+++ source/main/Divisions/Retrieve/RetrieveDivisionsDO.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewEnglandClassic.Divisions.Retrieve;
+internal class DataLayer : IDataLayer
+{
+    private readonly IRepository _repository;
+    
+    internal DataLayer(IConfiguration config)
+    {
+        _repository = new Repository(config);
+    }
+
+    /// <summary>
+    /// Unit Test Constructor
+    /// </summary>
+    /// <param name="repository"></param>
+    internal DataLayer(IRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public IEnumerable<Models.Division> ForTournament(Guid tournamentId)
+        => _repository.ForTournament(tournamentId).Select(division=> new Models.Division(division));
+}
+
+internal interface IDataLayer
+{
+    IEnumerable<Models.Division> ForTournament(Guid tournamentId);
+}
