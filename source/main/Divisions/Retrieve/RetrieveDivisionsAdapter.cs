b8c2a3221aef927e3e8799808869e8a09a5fa89f
Kipperman, David <kippermand@gmail.com>
2022-05-10 09:37:50 -0400

    Retrieve Divisions Adapter / tests

---
+++ source/main/Divisions/Retrieve/RetrieveDivisionsAdapter.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace NewEnglandClassic.Divisions.Retrieve;
+internal class Adapter : IAdapter
+{
+    private readonly IBusinessLogic _businessLogic;
+    
+    public Adapter(IConfiguration config)
+    {
+        _businessLogic = new BusinessLogic(config);
+    }
+
+    /// <summary>
+    /// Unit Test Constructor
+    /// </summary>
+    /// <param name="mockBusinessLogic"></param>
+    internal Adapter(IBusinessLogic mockBusinessLogic)
+    {
+        _businessLogic = mockBusinessLogic;
+    }
+
+    public IEnumerable<Models.ErrorDetail> Errors { get; private set; } = Enumerable.Empty<Models.ErrorDetail>();
+
+    public IEnumerable<IViewModel> ForTournament(Guid tournamentId)
+    {
+        var divisions = _businessLogic.ForTournament(tournamentId);
+
+        Errors = _businessLogic.Errors;
+
+        return divisions.Select(division => new ViewModel(division));
+    }
+}
+
+internal interface IAdapter
+{ 
+    IEnumerable<Models.ErrorDetail> Errors { get; }
+
+    IEnumerable<IViewModel> ForTournament(Guid tournamentId);
+}
