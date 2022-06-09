986103aee766cecdb097af63afd6ee0de18c92dd
Kipperman, David <kippermand@gmail.com>
2022-06-09 12:02:32 -0400

    get squads for tournament

--- source/tests/Squads/SquadsRepositoryTests.cs
+++ source/tests/Squads/SquadsRepositoryTests.cs
@@ -39,4 +39,42 @@ internal class Repository
 
         _dataContext.Verify(dataContext => dataContext.SaveChanges(), Times.Once());
     }
+
+    [Test]
+    public void ForTournament_ReturnsSquadsForSelectedTournament()
+    {
+        var tournamentId = Guid.NewGuid();
+
+        var squad1 = new NewEnglandClassic.Database.Entities.TournamentSquad
+        {
+            Id = Guid.NewGuid(),
+            TournamentId = tournamentId,
+            MaxPerPair = 1
+        };
+
+        var squad2 = new NewEnglandClassic.Database.Entities.TournamentSquad
+        {
+            Id = Guid.NewGuid(),
+            TournamentId = tournamentId,
+            MaxPerPair = 1
+        };
+
+        var squad3 = new NewEnglandClassic.Database.Entities.TournamentSquad
+        {
+            Id = Guid.NewGuid(),
+            TournamentId = Guid.NewGuid(),
+            MaxPerPair = 2
+        };
+
+        var squads = new[] { squad1, squad2, squad3 };
+        _dataContext.Setup(dataContext => dataContext.Squads).Returns(squads.SetUpDbContext());
+
+        var actual = _repository.ForTournament(tournamentId);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(actual.Count(), Is.EqualTo(2));
+            Assert.That(actual.Count(squad => squad.MaxPerPair == 1), Is.EqualTo(2));
+        });
+    }
 }
