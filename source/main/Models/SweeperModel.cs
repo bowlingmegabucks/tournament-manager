57699f01ad39390772289a1d1f8ec7fd176fd79b
Kipperman, David <kippermand@gmail.com>
2022-06-10 14:23:55 -0400

    Sweeper Entity Mapper

---
+++ source/main/Models/SweeperModel.cs
@@ -0,0 +1,22 @@
+﻿
+namespace NewEnglandClassic.Models;
+internal class Sweeper
+{
+    public Guid Id { get; set; }
+
+    public Guid TournamentId { get; set; }
+    
+    public decimal EntryFee { get; set; }
+
+    public short Games { get; set; }
+
+    public decimal CashRatio { get; set; }
+
+    public DateTime Date { get; set; }
+
+    public short MaxPerPair { get; set; }
+
+    public bool Complete { get; set; }
+
+    public IDictionary<Guid, int?> Divisions { get; set; } = new Dictionary<Guid, int?>();
+}
---
+++ source/main/Sweepers/Add/SweeperEntityMapper.cs
@@ -0,0 +1,28 @@
+﻿
+namespace NewEnglandClassic.Sweepers.Add;
+internal class EntityMapper : IEntityMapper
+{
+    public Database.Entities.SweeperSquad Execute(Models.Sweeper sweeper)
+        => new()
+        {
+            Id = sweeper.Id,
+            TournamentId = sweeper.TournamentId,
+            CashRatio = sweeper.CashRatio,
+            Date = sweeper.Date,
+            MaxPerPair = sweeper.MaxPerPair,
+            Complete = sweeper.Complete,
+            EntryFee = sweeper.EntryFee,
+            Games = sweeper.Games,
+            Divisions = sweeper.Divisions.Select(division => new Database.Entities.SweeperDivision
+            {
+                SweeperId = sweeper.Id,
+                DivisionId = division.Key,
+                BonusPinsPerGame = division.Value
+            }).ToList()
+        };
+}
+
+internal interface IEntityMapper
+{
+    Database.Entities.SweeperSquad Execute(Models.Sweeper sweeper);
+}
