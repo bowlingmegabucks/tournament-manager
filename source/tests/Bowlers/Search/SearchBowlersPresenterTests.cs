6ddddf11b45b10373c11b9611930ebd572054a96
Kipperman, David <kippermand@gmail.com>
2022-06-15 16:20:19 -0400

    search bowlers presenter / tests

---
+++ source/tests/Bowlers/Search/SearchBowlersPresenterTests.cs
@@ -0,0 +1,86 @@
+﻿
+namespace NewEnglandClassic.Tests.Bowlers.Search;
+
+[TestFixture]
+internal class Presenter
+{
+    private Mock<NewEnglandClassic.Bowlers.Search.IView> _view;
+    private Mock<NewEnglandClassic.Bowlers.Search.IAdapter> _adapter;
+
+    private NewEnglandClassic.Bowlers.Search.Presenter _presenter;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _view = new Mock<NewEnglandClassic.Bowlers.Search.IView>();
+        _adapter = new Mock<NewEnglandClassic.Bowlers.Search.IAdapter>();
+
+        _presenter = new NewEnglandClassic.Bowlers.Search.Presenter(_view.Object, _adapter.Object);
+    }
+
+    [Test]
+    public void Execute_AdapterExecute_CalledCorrectly()
+    {
+        var searchCritiera = new NewEnglandClassic.Models.BowlerSearchCriteria();
+        _view.SetupGet(view => view.SearchCriteria).Returns(searchCritiera);
+
+        _presenter.Execute();
+
+        _adapter.Verify(adapter => adapter.Execute(searchCritiera), Times.Once);
+    }
+
+    [Test]
+    public void Execute_AdapterHasError_ErrorFlow()
+    {
+        var error = new NewEnglandClassic.Models.ErrorDetail("error");
+        _adapter.SetupGet(adapter => adapter.Error).Returns(error);
+
+        var searchCritiera = new NewEnglandClassic.Models.BowlerSearchCriteria();
+        _view.SetupGet(view => view.SearchCriteria).Returns(searchCritiera);
+
+        _presenter.Execute();
+
+        Assert.Multiple(() =>
+        {
+            _view.Verify(view => view.DisplayError("error"), Times.Once);
+            _view.Verify(view => view.BindResults(It.IsAny<IEnumerable<NewEnglandClassic.Bowlers.Search.IViewModel>>()), Times.Never);
+        });
+    }
+
+    [Test]
+    public void Execute_AdapterHasNoError_ViewBindResults_CalledSorted()
+    {
+        var bowler1 = new Mock<NewEnglandClassic.Bowlers.Search.IViewModel>();
+        bowler1.SetupGet(bowler => bowler.LastName).Returns("Smith");
+        bowler1.SetupGet(bowler => bowler.FirstName).Returns("John");
+        bowler1.SetupGet(bowler => bowler.Id).Returns(Guid.NewGuid());
+
+        var bowler2 = new Mock<NewEnglandClassic.Bowlers.Search.IViewModel>();
+        bowler2.SetupGet(bowler => bowler.LastName).Returns("Doe");
+        bowler2.SetupGet(bowler => bowler.FirstName).Returns("John");
+        bowler2.SetupGet(bowler => bowler.Id).Returns(Guid.NewGuid());
+
+        var bowler3 = new Mock<NewEnglandClassic.Bowlers.Search.IViewModel>();
+        bowler3.SetupGet(bowler => bowler.LastName).Returns("Doe");
+        bowler3.SetupGet(bowler => bowler.FirstName).Returns("Jane");
+        bowler3.SetupGet(bowler => bowler.Id).Returns(Guid.NewGuid());
+
+        var bowlers = new[] { bowler1.Object, bowler2.Object, bowler3.Object };
+
+        _adapter.Setup(adapter => adapter.Execute(It.IsAny<NewEnglandClassic.Models.BowlerSearchCriteria>())).Returns(bowlers);
+
+        var searchCritiera = new NewEnglandClassic.Models.BowlerSearchCriteria();
+        _view.SetupGet(view => view.SearchCriteria).Returns(searchCritiera);
+
+        _presenter.Execute();
+
+        Assert.Multiple(() =>
+        {
+            _view.Verify(view => view.BindResults(It.Is<IEnumerable<NewEnglandClassic.Bowlers.Search.IViewModel>>(results => results.Count() == 3)), Times.Once);
+
+            _view.Verify(view => view.BindResults(It.Is<IEnumerable<NewEnglandClassic.Bowlers.Search.IViewModel>>(results => results.ToList()[0].Id == bowler3.Object.Id)), Times.Once);
+            _view.Verify(view => view.BindResults(It.Is<IEnumerable<NewEnglandClassic.Bowlers.Search.IViewModel>>(results => results.ToList()[1].Id == bowler2.Object.Id)), Times.Once);
+            _view.Verify(view => view.BindResults(It.Is<IEnumerable<NewEnglandClassic.Bowlers.Search.IViewModel>>(results => results.ToList()[2].Id == bowler1.Object.Id)), Times.Once);
+        });
+    }
+}
