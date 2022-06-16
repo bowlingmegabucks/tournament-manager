16bebbeb84965800e3a392f76f77960846459617
Kipperman, David <kippermand@gmail.com>
2022-06-15 20:13:36 -0400

    add bowler validator / tests

---
+++ source/main/Validators/PastDateValidator.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation.Validators;
+using FluentValidation;
+
+namespace NewEnglandClassic.Validators;
+internal class PastDate<T> : PropertyValidator<T, DateOnly>
+{
+    public override string Name => "PastDateValidator";
+
+    protected override string GetDefaultMessageTemplate(string errorCode)
+    => "Property {PropertyName} must be in the past";
+
+    public override bool IsValid(ValidationContext<T> context, DateOnly value)
+     => value is DateOnly date && date < DateOnly.FromDateTime(DateTime.Today);
+}
+
+internal static partial class Extensions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="ruleBuilder"></param>
+    /// <returns></returns>
+    internal static IRuleBuilderOptions<T, DateOnly> PastDate<T>(this IRuleBuilder<T, DateOnly> ruleBuilder)
+        => ruleBuilder.SetValidator(new PastDate<T>());
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="ruleBuilder"></param>
+    /// <returns></returns>
+    internal static IRuleBuilderOptions<T, DateOnly?> PastDate<T>(this IRuleBuilder<T, DateOnly?> ruleBuilder)
+        => ruleBuilder.SetValidator(new PastDate<T>());
+}
