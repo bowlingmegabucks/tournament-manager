77667277f676b7415c28c832fc28ac9ff7dec99f
Kipperman, David <kippermand@gmail.com>
2022-06-22 15:40:28 -0400

    Registration Model / tests

---
+++ source/main/Models/RegistrationModel.cs
@@ -0,0 +1,25 @@
+﻿
+namespace NewEnglandClassic.Models;
+internal class Registration
+{
+    public Bowler Bowler { get; set; }
+
+    public Division Division { get; set; }
+
+    public int? Average { get; set; }
+
+    public IEnumerable<Guid> Squads { get; set; }
+
+    public IEnumerable<Guid> Sweepers { get; set; }
+
+    public Registration(Guid bowlerId, Guid divisionId, IEnumerable<Guid> squads, IEnumerable<Guid> sweepers, int? average)
+    {
+        Bowler = new Bowler { Id = bowlerId };
+        Division = new Division { Id = divisionId };
+
+        Squads = squads;
+        Sweepers = sweepers;
+
+        Average = average;
+    }
+}
---
+++ source/tests/Models/RegistrationModelTests.cs
@@ -0,0 +1,84 @@
+﻿
+namespace NewEnglandClassic.Tests.Models;
+
+[TestFixture]
+internal class Registration
+{
+    [Test]
+    public void Constructor_BowlerInstanciatedWithId()
+    {
+        var bowlerId = Guid.NewGuid();
+        var divisionId = Guid.NewGuid();
+
+        var squads = new List<Guid> { Guid.NewGuid(), Guid.NewGuid() };
+        var sweepers = new List<Guid> { Guid.NewGuid(), Guid.NewGuid() };
+
+        var average = 200;
+
+        var registration = new NewEnglandClassic.Models.Registration(bowlerId, divisionId, squads, sweepers, average);
+
+        Assert.That(registration.Bowler.Id, Is.EqualTo(bowlerId));
+    }
+
+    [Test]
+    public void Constructor_DivisionInstanciatedWithId()
+    {
+        var bowlerId = Guid.NewGuid();
+        var divisionId = Guid.NewGuid();
+
+        var squads = new List<Guid> { Guid.NewGuid(), Guid.NewGuid() };
+        var sweepers = new List<Guid> { Guid.NewGuid(), Guid.NewGuid() };
+
+        var average = 200;
+
+        var registration = new NewEnglandClassic.Models.Registration(bowlerId, divisionId, squads, sweepers, average);
+
+        Assert.That(registration.Division.Id, Is.EqualTo(divisionId));
+    }
+
+    [Test]
+    public void Constructor_SquadsMapped()
+    {
+        var bowlerId = Guid.NewGuid();
+        var divisionId = Guid.NewGuid();
+
+        var squads = new List<Guid> { Guid.NewGuid(), Guid.NewGuid() };
+        var sweepers = new List<Guid> { Guid.NewGuid(), Guid.NewGuid() };
+
+        var average = 200;
+
+        var registration = new NewEnglandClassic.Models.Registration(bowlerId, divisionId, squads, sweepers, average);
+
+        Assert.That(registration.Squads, Is.EqualTo(squads));
+    }
+
+    [Test]
+    public void Constructor_SweepersMapped()
+    {
+        var bowlerId = Guid.NewGuid();
+        var divisionId = Guid.NewGuid();
+
+        var squads = new List<Guid> { Guid.NewGuid(), Guid.NewGuid() };
+        var sweepers = new List<Guid> { Guid.NewGuid(), Guid.NewGuid() };
+
+        var average = 200;
+
+        var registration = new NewEnglandClassic.Models.Registration(bowlerId, divisionId, squads, sweepers, average);
+
+        Assert.That(registration.Sweepers, Is.EqualTo(sweepers));
+    }
+
+    [Test]
+    public void Constructor_AverageMapped([Values(null, 200)] int? average)
+    {
+        var bowlerId = Guid.NewGuid();
+        var divisionId = Guid.NewGuid();
+
+        var squads = new List<Guid> { Guid.NewGuid(), Guid.NewGuid() };
+        var sweepers = new List<Guid> { Guid.NewGuid(), Guid.NewGuid() };
+
+        var registration = new NewEnglandClassic.Models.Registration(bowlerId, divisionId, squads, sweepers, average);
+
+        Assert.That(registration.Average, Is.EqualTo(average));
+    }
+}
