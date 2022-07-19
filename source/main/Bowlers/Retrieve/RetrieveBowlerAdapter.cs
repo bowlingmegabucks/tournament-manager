03971c850a908f367a3bc84b1dce0f63ad5aa598
Kipperman, David <kippermand@gmail.com>
2022-07-19 13:13:29 -0400

    retrieve bowler adapter
    added viewmodel

---
+++ source/main/Bowlers/Retrieve/RetrieveBowlerAdapter.cs
@@ -0,0 +1,37 @@
+﻿
+namespace NewEnglandClassic.Bowlers.Retrieve;
+internal class Adapter : IAdapter
+{
+    private readonly IBusinessLogic _businessLogic;
+
+    public Adapter()
+    {
+        _businessLogic = new BusinessLogic();
+    }
+
+    /// <summary>
+    /// Unit Test Constructor
+    /// </summary>
+    /// <param name="mockBusinessLogic"></param>
+    internal Adapter(IBusinessLogic mockBusinessLogic)
+    {
+        _businessLogic = mockBusinessLogic;
+    }
+
+    public Models.ErrorDetail? Error
+        => _businessLogic.Error;
+
+    public IViewModel? Execute(Guid bowlerId)
+    {
+        var bowler = _businessLogic.Execute(bowlerId);
+
+        return bowler != null ? new ViewModel(bowler) : null;
+    }
+}
+
+internal interface IAdapter
+{
+    Models.ErrorDetail? Error { get; }
+
+    IViewModel? Execute(Guid bowlerId);
+}
---
+++ source/main/Bowlers/Retrieve/RetrieveBowlerViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace NewEnglandClassic.Bowlers.Retrieve;
+internal class ViewModel : IViewModel
+{
+    public Guid Id { get; set; }
+
+    public ViewModel(Models.Bowler bowler)
+    {
+        Id = bowler.Id;
+    }
+}
+
+internal interface IViewModel
+{
+    Guid Id { get; }
+}
