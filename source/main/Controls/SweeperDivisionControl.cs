eb04a9fa2fb475a473d824fb45d620ec694dbb20
David Kipperman <kippermand@gmail.com>
2022-08-12 07:49:21 -0400

    sweeper division control update

--- source/main/Controls/SweeperDivisionControl.cs
+++ source/main/Controls/SweeperDivisionControl.cs
@@ -7,8 +7,8 @@ internal partial class SweeperDivisionControl : UserControl
     
     public int? BonusPinsPerGame
     {
-        get => NumericBonusPinsPerGame.Value == 0 ? null : (int)NumericBonusPinsPerGame.Value;
-        set => NumericBonusPinsPerGame.Value = value ?? 0;
+        get => bonusPinsPerGameValue.Value == 0 ? null : (int)bonusPinsPerGameValue.Value;
+        set => bonusPinsPerGameValue.Value = value ?? 0;
     }
     
     public SweeperDivisionControl(Divisions.IViewModel divisionViewModel)
@@ -16,18 +16,18 @@ internal partial class SweeperDivisionControl : UserControl
         InitializeComponent();
         
         DivisionId = divisionViewModel.Id;
-        TextboxDivisionName.Text = divisionViewModel.DivisionName;
+        nameText.Text = divisionViewModel.DivisionName;
     }
 
-    private void NumericBonusPinsPerGame_Validating(object sender, CancelEventArgs e)
+    private void BonusPinsPerGameValue_Validating(object sender, CancelEventArgs e)
     {
         if (BonusPinsPerGame < 0)
         {
             e.Cancel = true;
-            ErrorProviderSweeperDivision.SetError(NumericBonusPinsPerGame, "Bonus pins per game must be greater than or equal to 0.");
+            ErrorProviderSweeperDivision.SetError(bonusPinsPerGameValue, "Bonus pins per game must be greater than or equal to 0.");
         }
     }
 
-    private void NumericBonusPinsPerGame_Validated(object sender, EventArgs e) 
-        => ErrorProviderSweeperDivision.SetError(NumericBonusPinsPerGame, string.Empty);
+    private void BonusPinsPerGameValue_Validated(object sender, EventArgs e) 
+        => ErrorProviderSweeperDivision.SetError(bonusPinsPerGameValue, string.Empty);
 }
