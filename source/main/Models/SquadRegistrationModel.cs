524a9e975e337a6550d0e251d77f140fb6144eca
David Kipperman <kippermand@gmail.com>
2022-08-31 14:09:33 -0400

    squad registration model

---
+++ source/main/Models/SquadRegistrationModel.cs
@@ -0,0 +1,25 @@
+﻿
+namespace NortheastMegabuck.Models;
+internal class SquadRegistration
+{
+    public RegistrationId RegistrationId { get; }
+
+    public SquadId SquadId { get; }
+
+    public Bowler Bowler { get; }
+
+    public Division Division { get; }
+
+    public string LaneAssignment { get; }
+
+    public SquadRegistration(Database.Entities.SquadRegistration squadRegistration)
+    {
+        RegistrationId = squadRegistration.RegistrationId;
+        SquadId = squadRegistration.SquadId;
+
+        Bowler = new Bowler(squadRegistration.Registration.Bowler);
+        Division = new Division(squadRegistration.Registration.Division);
+
+        LaneAssignment = squadRegistration.LaneAssignment;
+    }
+}
---
+++ source/tests/Models/SquadRegistrationModelTests.cs
@@ -0,0 +1,50 @@
+﻿
+namespace NortheastMegabuck.Tests.Models;
+internal class SquadRegistration
+{
+    private NortheastMegabuck.Models.SquadRegistration _squadRegistration;
+
+    [OneTimeSetUp]
+    public void SetUp()
+    {
+        var entity = new NortheastMegabuck.Database.Entities.SquadRegistration
+        {
+            SquadId = SquadId.New(),
+            RegistrationId = RegistrationId.New(),
+            Registration = new NortheastMegabuck.Database.Entities.Registration
+            {
+                Bowler = new NortheastMegabuck.Database.Entities.Bowler
+                {
+                    Id = BowlerId.New()
+                },
+                Division = new NortheastMegabuck.Database.Entities.Division
+                {
+                    Id = NortheastMegabuck.Divisions.Id.New()
+                }
+            },
+            LaneAssignment = "12C"
+        };
+
+        _squadRegistration = new NortheastMegabuck.Models.SquadRegistration(entity);
+    }
+
+    [Test]
+    public void Constructor_RegistrationIdMapped()
+        => Assert.That(_squadRegistration.RegistrationId, Is.Not.EqualTo(RegistrationId.Empty));
+
+    [Test]
+    public void Constructor_SquadIdMapped()
+        => Assert.That(_squadRegistration.SquadId, Is.Not.EqualTo(SquadId.Empty));
+
+    [Test]
+    public void Constructor_BowlerMapped()
+        => Assert.That(_squadRegistration.Bowler.Id, Is.Not.EqualTo(BowlerId.Empty));
+
+    [Test]
+    public void Constructor_DivisionMapped()
+        => Assert.That(_squadRegistration.Division.Id, Is.Not.EqualTo(NortheastMegabuck.Divisions.Id.Empty));
+
+    [Test]
+    public void Constructor_LaneAssignmentMapped()
+        => Assert.That(_squadRegistration.LaneAssignment, Is.EqualTo("12C"));
+}
