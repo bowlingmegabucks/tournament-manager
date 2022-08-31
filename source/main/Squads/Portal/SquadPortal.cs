b9a588b30606da2c0d6ab17a3a690102e74ae65f
David Kipperman <kippermand@gmail.com>
2022-08-31 10:31:52 -0400

    load squad portal

---
+++ source/main/Squads/Portal/SquadPortal.cs
@@ -0,0 +1,25 @@
+﻿
+namespace NortheastMegabuck.Squads.Portal;
+public partial class Form : System.Windows.Forms.Form, IView
+{
+    private readonly IConfiguration _config;
+    private readonly SquadId _id;
+    public Form(IConfiguration config, SquadId id)
+    {
+        InitializeComponent();
+
+        _config = config;
+        _id = id;
+
+        new Presenter(config, this).Load();
+    }
+
+    public void SetPortalTitle(string title)
+        => Text = title;
+
+    SquadId IView.Id
+        => _id;
+
+    public void DisplayError(string message)
+        => MessageBox.Show(message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+}
---
+++ source/main/Squads/Portal/SquadPortalPresenter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NortheastMegabuck.Squads.Portal;
+internal class Presenter
+{
+    private readonly IView _view;
+    private readonly Retrieve.IAdapter _retrieveSquadAdapter;
+
+    public Presenter(IConfiguration config, IView view)
+    {
+        _view = view;
+
+        _retrieveSquadAdapter = new Retrieve.Adapter(config);
+    }
+
+    /// <summary>
+    /// Unit Test Constructor
+    /// </summary>
+    /// <param name="mockView"></param>
+    /// <param name="mockRetrieveSquadAdapter"></param>
+    internal Presenter(IView mockView, Retrieve.IAdapter mockRetrieveSquadAdapter)
+    {
+        _view = mockView;
+        _retrieveSquadAdapter = mockRetrieveSquadAdapter;
+    }
+
+    public void Load()
+    {
+        var squad = _retrieveSquadAdapter.Execute(_view.Id);
+
+        if (_retrieveSquadAdapter.Error != null)
+        {
+            _view.DisplayError(_retrieveSquadAdapter.Error.Message);
+
+            _view.Close();
+
+            return;
+        }
+
+        _view.SetPortalTitle($"{squad!.Date:MM/dd/yyyy hh:mmtt}");
+    }
+}
