3a81b79a186e3905a4bf296ba87aa9b5b6c9ff22
David Kipperman <kippermand@gmail.com>
2022-09-06 13:51:48 -0400

    clean up

--- source/main/LaneAssignments/Retrieve/RetrieveLaneAssignmentsAdapter.cs
+++ source/main/LaneAssignments/Retrieve/RetrieveLaneAssignmentsAdapter.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NortheastMegabuck.LaneAssignments.Retrieve;
+﻿namespace NortheastMegabuck.LaneAssignments.Retrieve;
 internal class Adapter : IAdapter
 {
     private readonly IBusinessLogic _businessLogic;
