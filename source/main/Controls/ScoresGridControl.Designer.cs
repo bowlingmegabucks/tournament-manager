21060e4ff23779e16a71ea02390cad7e8af6f51d
Kipperman, David <kippermand@gmail.com>
2022-10-01 19:52:09 -0400

    scores grid control
    just columns for number of games

---
+++ source/main/Controls/ScoresGridControl.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace NortheastMegabuck.Controls;
+
+partial class ScoresGrid
+{
+    /// <summary> 
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary> 
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Component Designer generated code
+
+    /// <summary> 
+    /// Required method for Designer support - do not modify 
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+            this.laneAssignmentColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bowlerNameColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.GridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GridView
+            // 
+            this.GridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.laneAssignmentColumn,
+            this.bowlerNameColumn});
+            this.GridView.RowTemplate.Height = 25;
+            // 
+            // laneAssignmentColumn
+            // 
+            this.laneAssignmentColumn.Frozen = true;
+            this.laneAssignmentColumn.HeaderText = "Lane";
+            this.laneAssignmentColumn.Name = "laneAssignmentColumn";
+            this.laneAssignmentColumn.ReadOnly = true;
+            this.laneAssignmentColumn.Width = 50;
+            // 
+            // bowlerNameColumn
+            // 
+            this.bowlerNameColumn.Frozen = true;
+            this.bowlerNameColumn.HeaderText = "Bowler";
+            this.bowlerNameColumn.Name = "bowlerNameColumn";
+            this.bowlerNameColumn.ReadOnly = true;
+            this.bowlerNameColumn.Width = 250;
+            // 
+            // ScoresGrid
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "ScoresGrid";
+            ((System.ComponentModel.ISupportInitialize)(this.GridView)).EndInit();
+            this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private DataGridViewTextBoxColumn laneAssignmentColumn;
+    private DataGridViewTextBoxColumn bowlerNameColumn;
+}
---
+++ source/main/Controls/ScoresGridControl.cs
@@ -0,0 +1,46 @@
+﻿
+namespace NortheastMegabuck.Controls;
+
+public partial class ScoresGrid
+#if DEBUG
+    : ScoresMiddleGrid
+#else
+    : Controls.DataGrid<Scores.IViewModel>
+#endif
+{
+    public ScoresGrid()
+    {
+        InitializeComponent();
+    }
+
+    public void GenerateGameColumns(int games)
+    {
+        for (var i = 1; i <= games; i++)
+        {
+            var column = new DataGridViewColumn()
+            {
+                Name = $"QualifyingScoreGame{i}",
+                ReadOnly = false,
+                Visible = true,
+                Width = 55,
+                HeaderText = $"Game {i}",
+                CellTemplate = new DataGridViewTextBoxCell()
+            };
+
+            column.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+
+            GridView.Columns.Add(column);
+        }
+    }
+}
+
+#if DEBUG
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+public class ScoresMiddleGrid : DataGrid<Scores.IViewModel>
+{
+    public ScoresMiddleGrid()
+    {
+
+    }
+}
+#endif
