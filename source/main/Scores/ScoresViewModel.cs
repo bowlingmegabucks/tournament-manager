269f136411f4906715824547955f2514ecd684e8
Kipperman, David <kippermand@gmail.com>
2022-10-01 19:26:21 -0400

    scores view model / tests

---
+++ source/main/Scores/ScoresViewModel.cs
@@ -0,0 +1,31 @@
+﻿
+namespace NortheastMegabuck.Scores;
+internal class ViewModel : IViewModel
+{
+    public BowlerId BowlerId { get; set; }
+
+    public string LaneAssignment { get; set; }
+
+    public string BowlerName { get; set; }
+
+    public IDictionary<int, int> Scores { get; set; }
+
+    public ViewModel(LaneAssignments.IViewModel laneAssignment)
+    {
+        BowlerId = laneAssignment.BowlerId;
+        LaneAssignment = laneAssignment.LaneAssignment;
+        BowlerName = laneAssignment.BowlerName;
+        Scores = new Dictionary<int, int>();
+    }
+}
+
+public interface IViewModel
+{ 
+    BowlerId BowlerId { get; set; }
+
+    string LaneAssignment { get; set; }
+
+    string BowlerName { get; set; }
+
+    IDictionary<int, int> Scores { get; set; }
+}
