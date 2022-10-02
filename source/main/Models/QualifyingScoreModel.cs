a60d2f3a9b9f0da371f72d2dbc37c94b6a2ef5e2
Kipperman, David <kippermand@gmail.com>
2022-10-01 21:55:16 -0400

    qualifying score model

---
+++ source/main/Models/QualifyingScoreModel.cs
@@ -0,0 +1,12 @@
+﻿
+namespace NortheastMegabuck.Models;
+internal class QualifyingScore
+{
+    public SquadId SquadId { get; init; }
+
+    public BowlerId BowlerId { get; init; }
+
+    public short GameNumber { get; init; }
+
+    public int Score { get; init; }
+}
