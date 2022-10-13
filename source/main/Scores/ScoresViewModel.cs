b2f26289d4974169630aa651b3c0840537f8d6ce
Kipperman, David <kippermand@gmail.com>
2022-10-12 21:23:44 -0400

    constructor for clipboard data / tests

--- source/main/Scores/ScoresViewModel.cs
+++ source/main/Scores/ScoresViewModel.cs
@@ -18,6 +18,24 @@ internal class ViewModel : IViewModel
         BowlerName = laneAssignment.BowlerName;
         Scores = new Dictionary<short, int>();
     }
+
+    public ViewModel(string clipboardData, short gamesPerSquad)
+    {
+        Scores = new Dictionary<short, int>();
+
+        var items = clipboardData.Split('\t');
+
+        LaneAssignment = items[0];
+        BowlerId = new BowlerId(new Guid(items[1]));
+        BowlerName = items[2];
+
+        short currentGame = 1;
+
+        for (var i = 5; currentGame <= gamesPerSquad && i < items.Length; i++)
+        {
+            Scores.Add(currentGame++, Convert.ToInt32(items[i]));
+        }
+    }
 }
 
 public interface IViewModel
--- source/tests/Scores/ScoresViewModelTests.cs
+++ source/tests/Scores/ScoresViewModelTests.cs
@@ -49,4 +49,88 @@ internal class ViewModel
 
         Assert.That(viewModel.Scores, Is.Empty);
     }
+
+    [Test]
+    public void Constructor_ClipboardString_LaneAssignmentMapped()
+    {
+        var data = "1A\t6c28c592-c241-401e-8414-251f658b8ae9\tBowler 1\t3\t0\t200\t201\t202\t203";
+
+        var viewModel = new NortheastMegabuck.Scores.ViewModel(data, 4);
+
+        Assert.That(viewModel.LaneAssignment, Is.EqualTo("1A"));
+    }
+
+    [Test]
+    public void Constructor_ClipboardString_BowlerIdMapped()
+    {
+        var data = "1A\t6c28c592-c241-401e-8414-251f658b8ae9\tBowler 1\t3\t0\t200\t201\t202\t203";
+
+        var viewModel = new NortheastMegabuck.Scores.ViewModel(data, 4);
+
+        Assert.That(viewModel.BowlerId, Is.EqualTo(new BowlerId(new Guid("6c28c592-c241-401e-8414-251f658b8ae9"))));
+    }
+
+    [Test]
+    public void Constructor_ClipboardString_BowlerNameMapped()
+    {
+        var data = "1A\t6c28c592-c241-401e-8414-251f658b8ae9\tBowler 1\t3\t0\t200\t201\t202\t203";
+
+        var viewModel = new NortheastMegabuck.Scores.ViewModel(data, 4);
+
+        Assert.That(viewModel.BowlerName, Is.EqualTo("Bowler 1"));
+    }
+
+    [Test]
+    public void Constructor_ClipboardString_ScoresEqualToGamesInSquad_ScoresMapped()
+    {
+        var data = "1A\t6c28c592-c241-401e-8414-251f658b8ae9\tBowler 1\t3\t0\t200\t201\t202\t203";
+
+        var viewModel = new NortheastMegabuck.Scores.ViewModel(data, 4);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(viewModel.Scores, Has.Count.EqualTo(4));
+
+            Assert.That(viewModel.Scores[1], Is.EqualTo(200));
+            Assert.That(viewModel.Scores[2], Is.EqualTo(201));
+            Assert.That(viewModel.Scores[3], Is.EqualTo(202));
+            Assert.That(viewModel.Scores[4], Is.EqualTo(203));
+        });
+    }
+
+    [Test]
+    public void Constructor_ClipboardString_ScoresGreaterThanGamesInSquad_ScoresMappedWithoutLastScore()
+    {
+        var data = "1A\t6c28c592-c241-401e-8414-251f658b8ae9\tBowler 1\t3\t0\t200\t201\t202\t203\t806";
+
+        var viewModel = new NortheastMegabuck.Scores.ViewModel(data, 4);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(viewModel.Scores, Has.Count.EqualTo(4));
+
+            Assert.That(viewModel.Scores[1], Is.EqualTo(200));
+            Assert.That(viewModel.Scores[2], Is.EqualTo(201));
+            Assert.That(viewModel.Scores[3], Is.EqualTo(202));
+            Assert.That(viewModel.Scores[4], Is.EqualTo(203));
+        });
+    }
+
+    [Test]
+    public void Constructor_ClipboardString_ScoresLessThanGamesInSquad_ScoresMapped()
+    {
+        var data = "1A\t6c28c592-c241-401e-8414-251f658b8ae9\tBowler 1\t3\t0\t200\t201\t202\t203";
+
+        var viewModel = new NortheastMegabuck.Scores.ViewModel(data, 5);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(viewModel.Scores, Has.Count.EqualTo(4));
+
+            Assert.That(viewModel.Scores[1], Is.EqualTo(200));
+            Assert.That(viewModel.Scores[2], Is.EqualTo(201));
+            Assert.That(viewModel.Scores[3], Is.EqualTo(202));
+            Assert.That(viewModel.Scores[4], Is.EqualTo(203));
+        });
+    }
 }
