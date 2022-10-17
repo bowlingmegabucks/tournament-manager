be8ac4f38c3591962f5343722576327cb9200d1f
Kipperman, David <kippermand@gmail.com>
2022-10-17 14:27:14 -0400

    retrieve scores adapter

---
+++ source/main/Scores/Retrieve/RetrieveScoresAdapter.cs
@@ -0,0 +1,33 @@
+﻿
+namespace NortheastMegabuck.Scores.Retrieve;
+internal class Adapter : IAdapter
+{
+    public Models.ErrorDetail? Error
+        => _businessLogic.Error;
+
+    private readonly IBusinessLogic _businessLogic;
+
+    public Adapter(IConfiguration config)
+    {
+        _businessLogic = new BusinessLogic(config);
+    }
+
+    /// <summary>
+    /// Unit Test Constructor
+    /// </summary>
+    /// <param name="mockBusinessLogic"></param>
+    internal Adapter(IBusinessLogic mockBusinessLogic)
+    {
+        _businessLogic = mockBusinessLogic;
+    }
+
+    public IEnumerable<IViewModel> Execute(SquadId squadId)
+        => _businessLogic.Execute(squadId).Select(squadScore => new ViewModel(squadScore));
+}
+
+internal interface IAdapter
+{
+    Models.ErrorDetail? Error { get; }
+
+    IEnumerable<IViewModel> Execute(SquadId squadId);
+}
