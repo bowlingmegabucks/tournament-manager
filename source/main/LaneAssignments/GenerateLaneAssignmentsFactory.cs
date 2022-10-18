3d573446c067d03248d1796eb53bf46f2c6c5f38
Kipperman, David <kippermand@gmail.com>
2022-10-18 10:24:39 -0400

    generate lane assignment factory / tests

---
+++ source/main/LaneAssignments/GenerateLaneAssignmentsFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NortheastMegabuck.LaneAssignments;
+internal class GenerateFactory
+{
+    public IGenerate Execute(bool staggeredSkip)
+        => staggeredSkip ? new StaggeredSkip() : new SameSkip();
+}
---
+++ source/tests/LaneAssignments/GenerateLaneAssignmentsFactoryTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NortheastMegabuck.Tests.LaneAssignments;
+
+[TestFixture]
+internal class GenerateFactory
+{
+    [Test]
+    public void Execute_StaggeredSkipTrue_ReturnsStaggeredSkip()
+    {
+        var instance = new NortheastMegabuck.LaneAssignments.GenerateFactory().Execute(true);
+
+        Assert.That(instance, Is.TypeOf<NortheastMegabuck.LaneAssignments.StaggeredSkip>());
+    }
+
+    [Test]
+    public void Execute_StaggeredSkipFalse_ReturnsSameSkip()
+    {
+        var instance = new NortheastMegabuck.LaneAssignments.GenerateFactory().Execute(false);
+
+        Assert.That(instance, Is.TypeOf<NortheastMegabuck.LaneAssignments.SameSkip>());
+    }
+}
