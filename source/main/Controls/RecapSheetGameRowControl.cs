3125b7910fee5e02500409e7a8ba98ff5ee15a4b
Kipperman, David <kippermand@gmail.com>
2022-10-18 11:09:34 -0400

    recap sheet score controls

---
+++ source/main/Controls/RecapSheetGameRowControl.cs
@@ -0,0 +1,24 @@
+﻿
+namespace NortheastMegabuck.Controls;
+public partial class RecapSheetGameRowControl : UserControl
+{
+    public RecapSheetGameRowControl()
+    {
+        InitializeComponent();
+
+        laneLabel.Text = string.Empty;
+        scoreLabel.Text = string.Empty;
+        handicapLabel.Text = string.Empty;
+        totalLabel.Text = string.Empty;
+    }
+
+    public short? Handicap
+    {
+        set => handicapLabel.Text = value.HasValue ? value.ToString() : "-";
+    }
+
+    public string LaneAssignment
+    {
+        set => laneLabel.Text = value;
+    }
+}
---
+++ source/main/Controls/RecapSheetGameTotalControl.cs
@@ -0,0 +1,18 @@
+﻿
+namespace NortheastMegabuck.Controls;
+public partial class RecapSheetGameTotalControl : UserControl
+{
+    public RecapSheetGameTotalControl()
+    {
+        InitializeComponent();
+
+        scratchScoreLabel.Text = string.Empty;
+        handicapLabel.Text = string.Empty;
+        handicapTotalLabel.Text = string.Empty;
+    }
+
+    public short? Handicap
+    {
+        set => handicapLabel.Text = value.HasValue ? value.ToString() : "-";
+    }
+}
