49f2ae001ba2fdc52938cf0852290d7580318a42
Kipperman, David <kippermand@gmail.com>
2022-10-19 12:46:48 -0400

    sweeper cut model

---
+++ source/main/Models/SquadCutModel.cs
@@ -0,0 +1,12 @@
+﻿
+namespace NortheastMegabuck.Models;
+internal class SweeperCut
+{
+    public SquadId SquadId { get; set; }
+
+    public IEnumerable<SquadScore> Scores { get; set; } = Enumerable.Empty<SquadScore>();
+
+    public int CutScore { get; set; }
+
+    public short CasherCount { get; set; }
+}
