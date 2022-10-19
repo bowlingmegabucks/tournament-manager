7b6c7c38cd0381cb051a801f00366eddb5ff9d52
Kipperman, David <kippermand@gmail.com>
2022-10-19 15:19:49 -0400

    sweeper cut view model / tests

---
+++ source/main/Sweepers/Cut/SweeperCutViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NortheastMegabuck.Sweepers.Cut;
+internal class ViewModel : IViewModel
+{
+    public short Place { get; }
+
+    public string BowlerName { get; }
+
+    public int Score { get; }
+
+    public int HighGame { get; }
+
+    public bool Casher { get; }
+
+    public ViewModel(Models.BowlerSquadScore bowlerScore, short place, int cashingPositions)
+    {
+        Place = place;
+        BowlerName = bowlerScore.Bowler.ToString();
+        Score = bowlerScore.Score;
+        HighGame = bowlerScore.HighGame;
+        Casher = place <= cashingPositions;
+    }
+}
+
+internal interface IViewModel
+{
+    short Place { get; }
+
+    string BowlerName { get; }
+
+    int Score { get; }
+
+    int HighGame { get; }
+
+    bool Casher { get; }
+}
---
+++ source/tests/Sweepers/Cut/SweeperCutViewModelTests.cs
@@ -0,0 +1,57 @@
+﻿
+namespace NortheastMegabuck.Tests.Sweepers.Cut;
+
+[TestFixture]
+internal class ViewModel
+{
+    [Test]
+    public void Constructor_ModelsBowlerSquadScorePlaceCashingPositions_PlaceMapped()
+    {
+        var viewModel = new NortheastMegabuck.Sweepers.Cut.ViewModel(new NortheastMegabuck.Models.BowlerSquadScore(200),1,5);
+
+        Assert.That(viewModel.Place, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void Constructor_ModelsBowlerSquadScorePlaceCashingPositions_BowlerNameMapped()
+    {
+        var viewModel = new NortheastMegabuck.Sweepers.Cut.ViewModel(new NortheastMegabuck.Models.BowlerSquadScore(200) 
+        { 
+            Bowler = new NortheastMegabuck.Models.Bowler { FirstName = "first", LastName = "last"} 
+        }, 1, 5);
+
+        Assert.That(viewModel.BowlerName, Is.EqualTo("first last"));
+    }
+
+    [Test]
+    public void Constructor_ModelsBowlerSquadScorePlaceCashingPositions_ScoreMapped()
+    {
+        var viewModel = new NortheastMegabuck.Sweepers.Cut.ViewModel(new NortheastMegabuck.Models.BowlerSquadScore(200,201), 1, 5);
+
+        Assert.That(viewModel.Score, Is.EqualTo(401));
+    }
+
+    [Test]
+    public void Constructor_ModelsBowlerSquadScorePlaceCashingPositions_HighGameMapped()
+    {
+        var viewModel = new NortheastMegabuck.Sweepers.Cut.ViewModel(new NortheastMegabuck.Models.BowlerSquadScore(200,201), 1, 5);
+
+        Assert.That(viewModel.HighGame, Is.EqualTo(201));
+    }
+
+    [Test]
+    public void Constructor_ModelsBowlerSquadScorePlaceCashingPositions_PlaceLessThanOrEqualToCashingPosition_CasherMapped([Range(1,5)]short place)
+    {
+        var viewModel = new NortheastMegabuck.Sweepers.Cut.ViewModel(new NortheastMegabuck.Models.BowlerSquadScore(200), place, 5);
+
+        Assert.That(viewModel.Casher, Is.True);
+    }
+
+    [Test]
+    public void Constructor_ModelsBowlerSquadScorePlaceCashingPositions_PlaceGreaterThanCashingPosition_CasherMapped()
+    {
+        var viewModel = new NortheastMegabuck.Sweepers.Cut.ViewModel(new NortheastMegabuck.Models.BowlerSquadScore(200), 6, 5);
+
+        Assert.That(viewModel.Casher, Is.False);
+    }
+}
