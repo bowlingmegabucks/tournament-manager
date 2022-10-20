6f24744fb641595588641098b388faa1797795d2
Kipperman, David <kippermand@gmail.com>
2022-10-20 11:06:42 -0400

    sweeper cut control

---
+++ source/main/Controls/Grids/SweeperCutControl.cs
@@ -0,0 +1,15 @@
+﻿
+namespace NortheastMegabuck.Controls.Grids;
+public partial class SweeperCutControl : UserControl
+{
+    internal SweeperCutControl(Sweepers.Cut.IViewModel model)
+    {
+        InitializeComponent();
+
+        placeLabel.Text = model.Place.ToString();
+        bowlerNameLabel.Text = model.BowlerName;
+        scoreLabel.Text = model.Score.ToString();
+        highGameLabel.Text = model.HighGame.ToString();
+        cashingPictureBox.Visible = model.Casher;
+    }
+}
