3bf6e036513e118ddff2c59b784cdd24f5980a42
Kipperman, David <kippermand@gmail.com>
2022-10-21 08:55:17 -0400

    complete sweeper repository

--- source/main/Sweepers/SweepersRepository.cs
+++ source/main/Sweepers/SweepersRepository.cs
@@ -32,6 +32,14 @@ internal class Repository : IRepository
 
     public Database.Entities.SweeperSquad Retrieve(SquadId id)
         => _dataContext.Sweepers.AsNoTracking().Single(sweeper => sweeper.Id == id);
+
+    public void Complete(SquadId id)
+    {
+        var sweeper = _dataContext.Sweepers.Single(sweeper => sweeper.Id == id);
+        sweeper.Complete = true;
+
+        _dataContext.SaveChanges();
+    }
 }
 
 internal interface IRepository
@@ -41,4 +49,6 @@ internal interface IRepository
     IEnumerable<Database.Entities.SweeperSquad> Retrieve(TournamentId tournamentId);
 
     Database.Entities.SweeperSquad Retrieve(SquadId id);
+
+    void Complete(SquadId id);
 }
