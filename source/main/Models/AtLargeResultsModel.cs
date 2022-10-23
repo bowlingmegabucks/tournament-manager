2a7d91a2469fb2c7a06e39d8f8aa047d60aa383d
Kipperman, David <kippermand@gmail.com>
2022-10-23 09:56:02 -0400

    add at large model (while thoughts are in mind about what needs to be in here)

---
+++ source/main/Models/AtLargeResultsModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NortheastMegabuck.Models;
+internal class AtLargeResults
+{
+    public Division Division { get; init; } = null!;
+
+    public int Entries { get; init; }
+
+    public IEnumerable<BowlerSquadScore> AdvancingScores { get; init; } = Enumerable.Empty<BowlerSquadScore>();
+
+    public IEnumerable<BowlerId> AdvancersWhoPreviouslyCashed { get; init; } = Enumerable.Empty<BowlerId>();
+}
