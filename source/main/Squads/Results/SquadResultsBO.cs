812db6d7db09de4d316ecda4976b75b2f222b2fc
Kipperman, David <kippermand@gmail.com>
2022-10-23 11:08:43 -0400

    add squadResultsBO (will add tests later)

---
+++ source/main/Squads/Results/SquadResultsBO.cs
@@ -0,0 +1,60 @@
+﻿
+namespace NortheastMegabuck.Squads.Results;
+internal class BusinessLogic
+{
+    public Models.ErrorDetail? Error { get; private set; }
+
+    private readonly Tournaments.Retrieve.IBusinessLogic _retrieveTournament;
+    private readonly ICalculator _squadResultCalculator;
+    private readonly Scores.Retrieve.IBusinessLogic _retrieveScores;
+
+    public BusinessLogic(IConfiguration config)
+    {
+        _retrieveTournament = new Tournaments.Retrieve.BusinessLogic(config);
+        _squadResultCalculator = new Calculator();
+        _retrieveScores = new Scores.Retrieve.BusinessLogic(config);
+    }
+
+    public IEnumerable<Models.SquadResult> Execute(SquadId squadId)
+    {
+        var tournament = _retrieveTournament.Execute(squadId);
+
+        if (_retrieveTournament.Error != null)
+        {
+            Error = _retrieveTournament.Error;
+
+            return Enumerable.Empty<Models.SquadResult>();
+        }
+
+        var scores = _retrieveScores.Execute(tournament!.Squads.Where(squad => squad.Date <= tournament.Squads.Single(s => s.Id == squadId).Date).Select(squad => squad.Id));
+
+        return Execute(scores, tournament).Where(result => result.Squad.Id == squadId).ToList();
+    }
+
+    private IEnumerable<Models.SquadResult> Execute(IEnumerable<Models.SquadScore> scores, Models.Tournament tournament)
+    {
+        var scoresBySquad = scores.GroupBy(score => score.SquadId).Select(group => new { Squad = tournament.Squads.Single(squad => squad.Id == group.Key), Scores = group }).OrderBy(scores => scores.Squad.Date).ToList();
+
+        var results = new List<Models.SquadResult>();
+
+        foreach (var scoresInSquad in scoresBySquad)
+        {
+            var scoresByDivision = scoresInSquad.Scores.GroupBy(score => score.Division);
+
+            foreach (var scoresInDivision in scoresByDivision)
+            {
+                var bowlerScores = scoresInDivision.GroupBy(score => score.Bowler).Select(bowlerScore => new Models.BowlerSquadScore(bowlerScore));
+
+                var result = _squadResultCalculator.Execute(
+                    scoresInSquad.Squad,
+                    scoresInDivision.Key,
+                    bowlerScores.ToList(),
+                    results.Where(result => result.Division == scoresInDivision.Key).SelectMany(result => result.AdvancingScores.Select(score => score.Bowler.Id)),
+                    scoresInSquad.Squad.FinalsRatio ?? tournament.FinalsRatio,
+                    scoresInSquad.Squad.CashRatio ?? tournament.CashRatio);
+            }
+        }
+
+        return results;
+    }
+}
