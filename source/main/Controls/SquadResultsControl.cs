9ec3261c3bb5ffc06eb09b6c89b9800856d740b6
Kipperman, David <kippermand@gmail.com>
2022-10-24 09:34:14 -0400

    squadresultscontrol

---
+++ source/main/Controls/SquadResultsControl.cs
@@ -0,0 +1,36 @@
+﻿
+namespace NortheastMegabuck.Controls;
+public partial class SquadResultsControl : UserControl
+{
+    internal SquadResultsControl(Squads.Results.IViewModel model)
+    {
+        InitializeComponent();
+
+        placeLabel.Text = model.Place.ToString();
+        bowlerNameLabel.Text = model.BowlerName;
+        scoreLabel.Text = model.Score.ToString();
+        highGameLabel.Text = model.HighGame.ToString();
+        advancerPictureBox.Visible = model.Advancer;
+        cashingPictureBox.Visible = model.Casher;
+
+        if (model.ScratchScore != model.Score)
+        {
+            scratchToolTip.SetToolTip(scoreLabel, $"{model.ScratchScore} Scratch");
+            scratchToolTip.SetToolTip(highGameLabel, $"{model.HighGameScratch} Scratch");
+        }
+    }
+
+    internal SquadResultsControl()
+    {
+        InitializeComponent();
+
+        placeLabel.Text = string.Empty;
+        bowlerNameLabel.Text = string.Empty;
+        scoreLabel.Text = string.Empty;
+        highGameLabel.Text = string.Empty;
+        advancerPictureBox.Visible = false;
+        cashingPictureBox.Visible = false;
+
+        Height = Convert.ToInt32(Math.Ceiling(Size.Height * 1.5));
+    }
+}
