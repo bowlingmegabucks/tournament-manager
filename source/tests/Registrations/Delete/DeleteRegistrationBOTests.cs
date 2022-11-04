0a0df9393626d629a1718607a1809d2c09609088
Kipperman, David <kippermand@gmail.com>
2022-11-04 08:55:10 -0400

    delete registration bo / tests

---
+++ source/tests/Registrations/Delete/DeleteRegistrationBOTests.cs
@@ -0,0 +1,40 @@
+﻿
+namespace NortheastMegabuck.Tests.Registrations.Delete;
+
+[TestFixture]
+internal class BusinessLogic
+{
+    private Mock<NortheastMegabuck.Registrations.Delete.IDataLayer> _dataLayer;
+
+    private NortheastMegabuck.Registrations.Delete.IBusinessLogic _businessLogic;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _dataLayer = new Mock<NortheastMegabuck.Registrations.Delete.IDataLayer>();
+
+        _businessLogic = new NortheastMegabuck.Registrations.Delete.BusinessLogic(_dataLayer.Object);
+    }
+
+    [Test]
+    public void Execute_BowlerIdSquadId_DataLayerExecute_CalledCorrectly()
+    {
+        var bowlerId = BowlerId.New();
+        var squadId = SquadId.New();
+
+        _businessLogic.Execute(bowlerId, squadId);
+
+        _dataLayer.Verify(dataLayer => dataLayer.Execute(bowlerId, squadId), Times.Once);
+    }
+
+    [Test]
+    public void Execute_BowlerIdSquadId_DataLayerExecuteThrowsException_ErrorMapped()
+    {
+        var ex = new Exception("exception");
+        _dataLayer.Setup(dataLayer => dataLayer.Execute(It.IsAny<BowlerId>(), It.IsAny<SquadId>())).Throws(ex);
+
+        _businessLogic.Execute(BowlerId.New(), SquadId.New());
+
+        Assert.That(_businessLogic.Error.Message, Is.EqualTo(ex.Message));
+    }
+}
