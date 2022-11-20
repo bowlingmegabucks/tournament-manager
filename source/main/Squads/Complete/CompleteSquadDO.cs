74b0a0c44d5379dbfe83d4a66063644a3f120744
David Kipperman <kippermand@gmail.com>
2022-11-20 17:01:05 -0500

    complete squad adapter/bo/do & tests

---
+++ source/main/Squads/Complete/CompleteSquadDO.cs
@@ -0,0 +1,28 @@
+﻿
+namespace NortheastMegabuck.Squads.Complete;
+internal class DataLayer : IDataLayer
+{
+    private readonly IRepository _repository;
+
+    public DataLayer(IConfiguration config)
+    {
+        _repository = new Repository(config);
+    }
+
+    /// <summary>
+    /// Unit Test Constructor
+    /// </summary>
+    /// <param name="mockRepository"></param>
+    internal DataLayer(IRepository mockRepository)
+    {
+        _repository = mockRepository;
+    }
+
+    public void Execute(SquadId id)
+        => _repository.Complete(id);
+}
+
+internal interface IDataLayer
+{
+    void Execute(SquadId id);
+}
