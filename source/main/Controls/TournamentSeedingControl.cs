d035eeddcd13c0dcfecb09b5efc0774fdc95e3b5
David Kipperman <kippermand@gmail.com>
2022-11-22 10:18:06 -0500

    tournament seeding control

---
+++ source/main/Controls/TournamentSeedingControl.cs
@@ -0,0 +1,26 @@
+﻿
+namespace NortheastMegabuck.Controls;
+public partial class TournamentSeedingControl : UserControl
+{
+    internal TournamentSeedingControl(Tournaments.Seeding.IViewModel model)
+    {
+        InitializeComponent();
+
+        seedLabel.Text = model.Seed.ToString();
+        bowlerNameLabel.Text = model.BowlerName;
+        scoreLabel.Text = model.Score.ToString();
+        highGameLabel.Text = model.HighGame.ToString();
+    }
+
+    internal TournamentSeedingControl()
+    {
+        InitializeComponent();
+
+        seedLabel.Text = string.Empty;
+        bowlerNameLabel.Text = string.Empty;
+        scoreLabel.Text = string.Empty;
+        highGameLabel.Text = string.Empty;
+
+        Height = Convert.ToInt32(Math.Ceiling(Size.Height * 1.5));
+    }
+}
