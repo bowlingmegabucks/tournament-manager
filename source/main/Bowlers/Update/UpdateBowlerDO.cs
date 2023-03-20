b3fd711b488c7d867bce60ef43f755a5e9d0107f
Kipperman, David <kippermand@gmail.com>
2023-03-20 10:38:16 -0400

    update bowler do for name

---
+++ source/main/Bowlers/Update/UpdateBowlerDO.cs
@@ -0,0 +1,28 @@
+﻿
+namespace NortheastMegabuck.Bowlers.Update;
+internal sealed class DataLayer : IDataLayer
+{
+    private readonly IRepository _repository;
+
+    public DataLayer(IConfiguration config)
+    {
+        _repository = new Repository(config);
+    }
+
+    /// <summary>
+    /// Unit Test Constructor
+    /// </summary>
+    /// <param name="mockRepository"></param>
+    internal DataLayer(IRepository mockRepository)
+    {
+        _repository = mockRepository;
+    }
+
+    void IDataLayer.Execute(BowlerId id, string firstName, string middleInitial, string lastName, string suffix)
+        => _repository.Update(id, firstName, middleInitial, lastName, suffix);
+}
+
+internal interface IDataLayer
+{
+    void Execute(BowlerId id, string firstName, string middleInitial, string lastName, string suffix);
+}
---
+++ source/tests/Bowlers/Update/UpdateBowlerDOTests.cs
@@ -0,0 +1,31 @@
+﻿namespace NortheastMegabuck.Tests.Bowlers.Update;
+
+[TestFixture]
+internal class DataLayer
+{
+    private Mock<NortheastMegabuck.Bowlers.IRepository> _repository;
+
+    private NortheastMegabuck.Bowlers.Update.IDataLayer _dataLayer;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _repository = new Mock<NortheastMegabuck.Bowlers.IRepository>();
+
+        _dataLayer = new NortheastMegabuck.Bowlers.Update.DataLayer(_repository.Object);
+    }
+
+    [Test]
+    public void Execute_BowlerName_RepositoryUpdate_CalledCorrectly()
+    {
+        var id = BowlerId.New();
+        var firstName = "firstName";
+        var middleInitial = "middleInitial";
+        var lastName = "lastName";
+        var suffix = "suffix";
+
+        _dataLayer.Execute(id, firstName, middleInitial, lastName, suffix);
+
+        _repository.Verify(repository => repository.Update(id, firstName, middleInitial, lastName, suffix), Times.Once);
+    }
+}
