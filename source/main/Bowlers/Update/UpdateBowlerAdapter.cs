fe06a55e66f7ec048aa938ea90da634c563c29b4
Kipperman, David <kippermand@gmail.com>
2023-03-21 09:36:57 -0400

    update bowler adapter / tests

---
+++ source/main/Bowlers/Update/UpdateBowlerAdapter.cs
@@ -0,0 +1,33 @@
+﻿
+namespace NortheastMegabuck.Bowlers.Update;
+internal class Adapter : IAdapter
+{
+    public IEnumerable<Models.ErrorDetail> Errors
+        => _businessLogic.Errors;
+
+    private readonly IBusinessLogic _businessLogic;
+
+    public Adapter(IConfiguration config)
+    {
+        _businessLogic = new BusinessLogic(config);
+    }
+
+    /// <summary>
+    /// Unit Test Constructor
+    /// </summary>
+    /// <param name="mockBusinessLogic"></param>
+    internal Adapter(IBusinessLogic mockBusinessLogic)
+    {
+        _businessLogic = mockBusinessLogic;
+    }
+
+    void IAdapter.Execute(INameViewModel viewModel)
+        => _businessLogic.Execute(viewModel.Id, new Models.PersonName(viewModel));
+}
+
+internal interface IAdapter
+{
+    IEnumerable<Models.ErrorDetail> Errors { get; }
+
+    void Execute(INameViewModel viewModel);
+}
---
+++ source/tests/Bowlers/Update/UpdateBowlerAdapterTests.cs
@@ -0,0 +1,46 @@
+﻿
+namespace NortheastMegabuck.Tests.Bowlers.Update;
+
+[TestFixture]
+internal class Adapter
+{
+    private Mock<NortheastMegabuck.Bowlers.Update.IBusinessLogic> _businessLogic;
+
+    private NortheastMegabuck.Bowlers.Update.IAdapter _adapter;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _businessLogic = new Mock<NortheastMegabuck.Bowlers.Update.IBusinessLogic>();
+
+        _adapter = new NortheastMegabuck.Bowlers.Update.Adapter(_businessLogic.Object);
+    }
+
+    [Test]
+    public void Execute_INameViewModel_BusinessLogicExecute_CalledCorrectly()
+    {
+        var bowlerId = BowlerId.New();
+        var firstName = "firstName";
+
+        var personName = new Mock<NortheastMegabuck.Bowlers.Update.INameViewModel>();
+        personName.SetupGet(p => p.Id).Returns(bowlerId);
+        personName.SetupGet(p => p.FirstName).Returns(firstName);
+
+        _adapter.Execute(personName.Object);
+
+        _businessLogic.Verify(businessLogic => businessLogic.Execute(bowlerId, It.Is<NortheastMegabuck.Models.PersonName>(name => name.First == "firstName")), Times.Once);
+    }
+
+    [Test]
+    public void Execute_INameViewModel_ErrorDetailSetToBusinessLogic()
+    {
+        var errors = Enumerable.Repeat(new NortheastMegabuck.Models.ErrorDetail("error"), 3);
+        _businessLogic.SetupGet(businessLogic => businessLogic.Errors).Returns(errors);
+
+        var personName = new Mock<NortheastMegabuck.Bowlers.Update.INameViewModel>().Object;
+
+        _adapter.Execute(personName);
+
+        Assert.That(_adapter.Errors, Is.EqualTo(errors));
+    }
+}
