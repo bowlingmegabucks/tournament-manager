f7327ab20ef488aaa020fea31e085217309cee63
Kipperman, David <kippermand@gmail.com>
2023-03-21 15:31:43 -0400

    retrieve bowler bo / tests
    updated dependencies to take in IConfiguration

--- source/main/Bowlers/Retrieve/RetrieveBowlersBO.cs
+++ source/main/Bowlers/Retrieve/RetrieveBowlersBO.cs
@@ -4,13 +4,35 @@ internal class BusinessLogic : IBusinessLogic
 {
     public Models.ErrorDetail? Error { get; private set; }
 
-    internal BusinessLogic()
-    {
+    private readonly IDataLayer _dataLayer;
 
+    public BusinessLogic(IConfiguration config)
+    {
+        _dataLayer = new DataLayer(config);
+    }
+
+    /// <summary>
+    /// Unit Test Constructor
+    /// </summary>
+    /// <param name="mockDataLayer"></param>
+    internal BusinessLogic(IDataLayer mockDataLayer)
+    {
+        _dataLayer = mockDataLayer;
     }
 
     public Models.Bowler? Execute(BowlerId id)
-        => null;
+    {
+        try
+        {
+            return _dataLayer.Execute(id);
+        }
+        catch (Exception ex)
+        {
+            Error = new Models.ErrorDetail(ex);
+
+            return null;
+        }
+    }        
 }
 
 internal interface IBusinessLogic
---
+++ source/tests/Bowlers/Retrieve/RetrieveBowlersBOTests.cs
@@ -0,0 +1,55 @@
+﻿
+namespace NortheastMegabuck.Tests.Bowlers.Retrieve;
+
+[TestFixture]
+internal class BusinessLogic
+{
+    private Mock<NortheastMegabuck.Bowlers.Retrieve.IDataLayer> _dataLayer;
+
+    private NortheastMegabuck.Bowlers.Retrieve.IBusinessLogic _businessLogic;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _dataLayer = new Mock<NortheastMegabuck.Bowlers.Retrieve.IDataLayer>();
+
+        _businessLogic = new NortheastMegabuck.Bowlers.Retrieve.BusinessLogic(_dataLayer.Object);
+    }
+
+    [Test]
+    public void Execute_BowlerId_DataLayerExecute_CalledCorrectly()
+    {
+        var bowlerId = BowlerId.New();
+
+        _businessLogic.Execute(bowlerId);
+
+        _dataLayer.Verify(dataLayer => dataLayer.Execute(bowlerId), Times.Once);
+    }
+
+    [Test]
+    public void Execute_BowlerId_DataLayerExecuteSuccessful_ReturnsBowler()
+    {
+        var bowler = new NortheastMegabuck.Models.Bowler();
+        _dataLayer.Setup(dataLayer => dataLayer.Execute(It.IsAny<BowlerId>())).Returns(bowler);
+
+        var actual = _businessLogic.Execute(BowlerId.New());
+
+        Assert.That(actual, Is.EqualTo(bowler));
+    }
+
+    [Test]
+    public void Execute_BowlerId_DataLayerExecuteThrowsException_ExceptionFlow()
+    {
+        var ex = new Exception("ex");
+        _dataLayer.Setup(dataLayer => dataLayer.Execute(It.IsAny<BowlerId>())).Throws(ex);
+
+        var actual = _businessLogic.Execute(BowlerId.New());
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(actual, Is.Null);
+
+            Assert.That(_businessLogic.Error.Message, Is.EqualTo("ex"));
+        });
+    }
+}
