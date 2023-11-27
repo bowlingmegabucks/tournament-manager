9c01276b08a88383c7c3fb48f6f78c6d07c16353
Kipperman, David <kippermand@gmail.com>
2023-11-27 11:46:16 -0500

    update registration do

--- source/main/Registrations/Update/UpdateRegistrationDO.cs
+++ source/main/Registrations/Update/UpdateRegistrationDO.cs
@@ -21,9 +21,14 @@ internal class DataLayer : IDataLayer
 
     public async Task ExecuteAsync(RegistrationId id, bool superSweeper, CancellationToken cancellationToken)
         => await _repository.UpdateAsync(id, superSweeper, cancellationToken).ConfigureAwait(false);
+
+    public async Task ExecuteAsync(RegistrationId id, DivisionId divisionId, int? average, DateOnly? dateOfBirth, CancellationToken cancellationToken)
+        => await _repository.UpdateAsync(id, divisionId, average, dateOfBirth, cancellationToken).ConfigureAwait(false);
 }
 
 internal interface IDataLayer
 {
     Task ExecuteAsync(RegistrationId id, bool superSweeper, CancellationToken cancellationToken);
+
+    Task ExecuteAsync(RegistrationId id, DivisionId divisionId, int? average, DateOnly? dateOfBirth, CancellationToken cancellationToken);
 }
