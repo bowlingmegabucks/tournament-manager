9caeda2d7176048fd80e9707b665e943396df305
Kipperman, David <kippermand@gmail.com>
2023-11-27 11:52:03 -0500

    add update registration view model

---
+++ source/main/Registrations/Update/UpdateRegistrationModel.cs
@@ -0,0 +1,13 @@
+﻿
+namespace NortheastMegabuck.Registrations.Update;
+
+internal class UpdateRegistrationModel
+{
+    public required Models.Division Division { get; init; }
+
+    public DateOnly? DateOfBirth { get; init; }
+
+    public int? Average { get; init; }
+
+    public required DateOnly TournamentStartDate { get; init; }
+}
