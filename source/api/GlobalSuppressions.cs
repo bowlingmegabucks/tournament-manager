a79df99893f5121ccdf83a029db6dfdbc3d3a630
David Kipperman <kippermand@gmail.com>
2025-07-09 15:01:26 -0400

    Remove unused PingController and related files; update project dependencies

--- source/api/GlobalSuppressions.cs
+++
@@ -1,5 +0,0 @@
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage(
-    "Performance",
-    "CA1515: Consider making public types internal",
-    Scope = "namespaceanddescendants",
-    Target = "NortheastMegabuck.Api.Controllers")]
--- source/api/Program.cs
+++ source/api/Program.cs
@@ -1,8 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -11,7 +8,5 @@ var app = builder.Build();
 app.MapOpenApi();
 
 app.UseHttpsRedirection();
-
-app.MapControllers();
 
 await app.RunAsync();
