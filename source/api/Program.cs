5862e52436efa1917efe3cfd2459c6d80512ad98
David Kipperman <kippermand@gmail.com>
2025-07-16 18:54:47 -0400

    Add OpenTelemetry integration for enhanced observability
    This commit introduces OpenTelemetry packages to the project, enabling telemetry features in the application. Key updates include:
    
    - Added OpenTelemetry packages in `Directory.Packages.props` and `NortheastMegabuck.Api.csproj`.
    - Configured OpenTelemetry services in `Program.cs` for tracing and metrics instrumentation.
    - Updated `launchSettings.json` with environment variables for OTLP endpoint and protocol.
    - Modified `docker-compose.yml` to include a new Seq service for log collection and visualization.
    
    These changes significantly improve the application's monitoring and logging capabilities.

--- source/api/Program.cs
+++ source/api/Program.cs
@@ -8,6 +8,10 @@ using NortheastMegabuck.Api;
 using NortheastMegabuck.Api.Authentication;
 using NortheastMegabuck.Database;
 using NortheastMegabuck.Models;
+using OpenTelemetry;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -66,6 +70,23 @@ builder.Services.SwaggerDocument(o =>
 
     o.SerializerSettings = s
         => s.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+});
+
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(resource => resource.AddService(builder.Environment.ApplicationName))
+    .WithTracing(tracing => tracing
+        .AddHttpClientInstrumentation()
+        .AddAspNetCoreInstrumentation())
+    .WithMetrics(metrics => metrics
+        .AddHttpClientInstrumentation()
+        .AddAspNetCoreInstrumentation()
+        .AddRuntimeInstrumentation())
+    .UseOtlpExporter();
+
+builder.Logging.AddOpenTelemetry(options =>
+{
+    options.IncludeScopes = true;
+    options.IncludeFormattedMessage = true;
 });
 
 var app = builder.Build();
