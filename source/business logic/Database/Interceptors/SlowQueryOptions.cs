eef250c788695dda8e2f9f8f36c912983c8cd716
David Kipperman <kippermand@gmail.com>
2025-07-21 20:52:34 -0400

    Update SlowQueryOptions to use init accessor for ThresholdMilliseconds

--- source/business logic/Database/Interceptors/SlowQueryOptions.cs
+++ source/business logic/Database/Interceptors/SlowQueryOptions.cs
@@ -2,5 +2,5 @@ namespace BowlingMegabucks.TournamentManager.Database.Interceptors;
 
 internal sealed class SlowQueryOptions
 {
-    public int ThresholdMilliseconds { get; set; }
+    public int ThresholdMilliseconds { get; init; } = 250;
 }
