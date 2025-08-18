1cfd1a4aecd0d93fcc4c084145b7e265b7213c06
David Kipperman <kippermand@gmail.com>
2025-08-18 12:17:49 -0400

    Remove Id property from UpdateRegistrationRequest
    Eliminated the Id property and its documentation from UpdateRegistrationRequest, as it is now expected to be provided via the route parameter rather than the request body.

--- source/api/Registrations/UpdateRegistration/UpdateRegistrationRequest.cs
+++ source/api/Registrations/UpdateRegistration/UpdateRegistrationRequest.cs
@@ -8,11 +8,6 @@ namespace BowlingMegabucks.TournamentManager.Api.Registrations.UpdateRegistratio
 public sealed record UpdateRegistrationRequest
 {
     /// <summary>
-    /// The ID of the registration to update.  This comes from the route parameter.
-    /// </summary>
-    public required RegistrationId Id { get; init; }
-
-    /// <summary>
     /// The registration to update.
     /// </summary>
     public required UpdateRegistrationInput Registration { get; init; }
