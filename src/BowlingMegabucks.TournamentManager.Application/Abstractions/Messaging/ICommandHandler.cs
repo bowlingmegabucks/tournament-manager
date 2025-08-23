8b57009f237454216cac008c08381b5cf47548c2
David Kipperman <kippermand@gmail.com>
2025-08-23 11:45:00 -0400

    Rename Handle to HandleAsync in ICommandHandler interfaces
    Updated method names from Handle to HandleAsync in ICommandHandler interfaces to better reflect asynchronous operation conventions.

--- src/BowlingMegabucks.TournamentManager.Application/Abstractions/Messaging/ICommandHandler.cs
+++ src/BowlingMegabucks.TournamentManager.Application/Abstractions/Messaging/ICommandHandler.cs
@@ -5,11 +5,11 @@ namespace BowlingMegabucks.TournamentManager.Application.Abstractions.Messaging;
 internal interface ICommandHandler<TCommand>
     where TCommand : ICommand
 {
-    Task<ErrorOr<Success>> Handle(TCommand command, CancellationToken cancellationToken);
+    Task<ErrorOr<Success>> HandleAsync(TCommand command, CancellationToken cancellationToken);
 }
 
 internal interface ICommandHandler<TCommand, TResponse>
      where TCommand : ICommand<TResponse>
 {
-    Task<ErrorOr<TResponse>> Handle(TCommand command, CancellationToken cancellationToken);
+    Task<ErrorOr<TResponse>> HandleAsync(TCommand command, CancellationToken cancellationToken);
 }
