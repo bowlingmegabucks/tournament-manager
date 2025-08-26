82a8ebc3c84e0716d368d184d37c226eaafc3e25
David Kipperman <kippermand@gmail.com>
2025-08-26 15:15:38 -0400

    Suppress CA1812 warning in ApplicationDbContext
    Added SuppressMessage attribute to ApplicationDbContext to avoid CA1812 warning about uninstantiated internal classes. Justification provided as the class is instantiated by the dependency injection container.

--- src/BowlingMegabucks.TournamentManager.Infrastructure/Database/ApplicationDbContext.cs
+++ src/BowlingMegabucks.TournamentManager.Infrastructure/Database/ApplicationDbContext.cs
@@ -1,7 +1,12 @@
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.EntityFrameworkCore;
 
 namespace BowlingMegabucks.TournamentManager.Infrastructure.Database;
 
+[SuppressMessage(
+    "Performance",
+    "CA1812:Avoid uninstantiated internal classes",
+    Justification = "Instantiated by dependency injection container.")]
 internal sealed class ApplicationDbContext
     : DbContext
 {
