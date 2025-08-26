f077c7f5b0582e459f648b087a4216c52968f5cf
David Kipperman <kippermand@gmail.com>
2025-08-26 16:01:03 -0400

    Add AuditInterceptor to DbContext configuration
    Registered AuditInterceptor as a scoped service and included it in the ApplicationDbContext's interceptors. This enables auditing functionality alongside slow query interception.

--- src/BowlingMegabucks.TournamentManager.Infrastructure/Database/DatabaseExtensions.cs
+++ src/BowlingMegabucks.TournamentManager.Infrastructure/Database/DatabaseExtensions.cs
@@ -12,10 +12,14 @@ internal static class DatabaseExtensions
         services.Configure<SlowQueryOptions>(config.GetSection("QueryPerformance"));
         services.AddScoped<SlowQueryInterceptor>();
 
+        services.AddScoped<AuditInterceptor>();
+
         services.AddDbContext<ApplicationDbContext>((sp, options) => options
             .UseMySql(config.GetConnectionString("TournamentManager") ?? throw new InvalidOperationException("Cannot get connection string TournamentManager"),
                 new MySqlServerVersion(new Version(11, 4, 7)), mySqlOptions => mySqlOptions.EnableRetryOnFailure(3))
-            .AddInterceptors(sp.GetRequiredService<SlowQueryInterceptor>()));
+            .AddInterceptors(
+                sp.GetRequiredService<AuditInterceptor>(),
+                sp.GetRequiredService<SlowQueryInterceptor>()));
 
         return services;
     }
