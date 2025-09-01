2b320386608f63611d07f8c17028e7e2b37e028d
David Kipperman <kippermand@gmail.com>
2025-09-01 11:17:16 -0400

    Remove unused tournament retrieval logic from Adapter
    Eliminated dependencies and methods related to retrieving tournaments by ID and listing tournaments from the Adapter class, simplifying its responsibilities to only handle squad-based retrieval.

--- source/presentation/Tournaments/Retrieve/RetrieveTournamentsAdapter.cs
+++ source/presentation/Tournaments/Retrieve/RetrieveTournamentsAdapter.cs
@@ -1,45 +1,16 @@
-﻿using BowlingMegabucks.TournamentManager.Abstractions.Messaging;
-using BowlingMegabucks.TournamentManager.Models;
-using BowlingMegabucks.TournamentManager.Tournaments.GetTournamentById;
-using BowlingMegabucks.TournamentManager.Tournaments.GetTournaments;
+﻿namespace BowlingMegabucks.TournamentManager.Tournaments.Retrieve;
 
-namespace BowlingMegabucks.TournamentManager.Tournaments.Retrieve;
 internal class Adapter : IAdapter
 {
     private readonly Lazy<IBusinessLogic> _businessLogic;
-    private IBusinessLogic BusinessLogic => _businessLogic.Value;
-
-    private readonly Lazy<IQueryHandler<GetTournamentsQuery, IEnumerable<Models.Tournament>>> _queryHandler;
-    private IQueryHandler<GetTournamentsQuery, IEnumerable<Models.Tournament>> QueryHandler
-        => _queryHandler.Value;
-
-    private readonly Lazy<IQueryHandler<GetTournamentByIdQuery, Models.Tournament?>> _retrieveTournament;
-    private IQueryHandler<GetTournamentByIdQuery, Models.Tournament?> RetrieveTournament
-        => _retrieveTournament.Value;
+    private IBusinessLogic BusinessLogic
+        => _businessLogic.Value;
 
     public Models.ErrorDetail? Error { get; private set; }
 
-    public Adapter(IBusinessLogic businessLogic, 
-        IQueryHandler<GetTournamentsQuery, IEnumerable<Models.Tournament>> queryHandler,
-        IQueryHandler<GetTournamentByIdQuery, Models.Tournament?> retrieveTournament)
+    public Adapter(IBusinessLogic businessLogic)
     {
-        _queryHandler = new Lazy<IQueryHandler<GetTournamentsQuery, IEnumerable<Models.Tournament>>>(() => queryHandler);
         _businessLogic = new Lazy<IBusinessLogic>(() => businessLogic);
-        _retrieveTournament = new Lazy<IQueryHandler<GetTournamentByIdQuery, Models.Tournament?>>(() => retrieveTournament);
-    }
-
-    public async Task<IViewModel?> ExecuteAsync(TournamentId tournamentId, CancellationToken cancellationToken)
-    {
-        var tournamentResult = await RetrieveTournament.HandleAsync(new() { Id = tournamentId }, cancellationToken).ConfigureAwait(false);
-
-        if (tournamentResult.IsError)
-        {
-            Error = tournamentResult.FirstError.ToErrorDetail();
-
-            return null;
-        }
-
-        return new ViewModel(tournamentResult.Value!);
     }
 
     public async Task<IViewModel?> ExecuteAsync(SquadId squadId, CancellationToken cancellationToken)
@@ -55,8 +26,6 @@ internal class Adapter : IAdapter
 internal interface IAdapter
 {
     Models.ErrorDetail? Error { get; }
-
-    Task<IViewModel?> ExecuteAsync(TournamentId tournamentId, CancellationToken cancellationToken);
 
     Task<IViewModel?> ExecuteAsync(SquadId squadId, CancellationToken cancellationToken);
 }
