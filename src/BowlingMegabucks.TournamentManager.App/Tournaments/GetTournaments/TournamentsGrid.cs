0b7ddc37a2bd0195ec43ab65bb2b2018d808dc10
David Kipperman <kippermand@gmail.com>
2025-09-01 13:57:18 -0400

    Update base classes for TournamentsGrid
    Changed the base class references for TournamentsGrid to use fully qualified names. This improves clarity and resolves potential ambiguity in class inheritance.

--- src/BowlingMegabucks.TournamentManager.App/Tournaments/GetTournaments/TournamentsGrid.cs
+++ src/BowlingMegabucks.TournamentManager.App/Tournaments/GetTournaments/TournamentsGrid.cs
@@ -1,13 +1,12 @@
-using BowlingMegabucks.TournamentManager.App.Tournaments.GetTournaments;
 using BowlingMegabucks.TournamentManager.Presentation.Tournaments.GetTournaments;
 
 namespace BowlingMegabucks.TournamentManager.App.Tournaments;
 
 internal sealed partial class TournamentsGrid
 #if DEBUG   
-    : TournamentMiddleGrid
+    : Tournaments.GetTournaments.TournamentMiddleGrid
 #else
-    : DataGridControl<TournamentSummaryViewModel>
+    : Controls.Grids.DataGridControl<TournamentSummaryViewModel>
 #endif
 {
     public TournamentsGrid()
